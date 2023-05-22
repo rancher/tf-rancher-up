fbf60a8472c5b1dcd2ba6c34fe4303fbfd0ec653
dkeightley <derek@rancher.com>
2023-05-22 14:24:29 +1200

    Add rancher2_bootstrap for further use in other modules

--- modules/rancher/outputs.tf
+++ modules/rancher/outputs.tf
@@ -4,7 +4,13 @@ output "rancher_hostname" {
 }
 
 output "rancher_bootstrap_password" {
-  description = "Password to use for bootstrapping Rancher"
+  description = "Password for the Rancher admin account"
   value       = var.rancher_bootstrap_password
+  sensitive   = true
+}
+
+output "rancher_token" {
+  description = "Rancher API token for the admin user"
+  value       = rancher2_bootstrap.admin[0].token
   sensitive   = true
 }
