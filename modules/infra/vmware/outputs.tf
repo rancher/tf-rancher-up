6e69435954731216b83a04362ff6521c163a60d2
Murali Paluru <leodotcloud@gmail.com>
2023-06-05 23:38:39 +0530

    fix formating issues

--- modules/infra/vmware/outputs.tf
+++ modules/infra/vmware/outputs.tf
@@ -1,9 +1,9 @@
 output "vsphere_virtual_machine" {
-    value = vsphere_virtual_machine.instance[*].guest_ip_addresses
-    description = "VM IPs"
+  value       = vsphere_virtual_machine.instance[*].guest_ip_addresses
+  description = "VM IPs"
 }
 
 output "rancher_ip" {
-  value = vsphere_virtual_machine.instance[0].guest_ip_addresses[0]
+  value       = vsphere_virtual_machine.instance[0].guest_ip_addresses[0]
   description = "This output will get IP from the first VM on which Rancher will be installed."
 }
--- recipes/upstream/vmware/rke/main.tf
+++ recipes/upstream/vmware/rke/main.tf
@@ -6,13 +6,13 @@ provider "vsphere" {
 }
 
 module "upstream-cluster" {
-  source = "../../../../modules/infra/vmware"
-  ssh_private_key_path  = var.ssh_private_key_path
-  vsphere_datacenter = var.vsphere_datacenter
-  vsphere_resource_pool = var.vsphere_resource_pool
-  vsphere_datastore = var.vsphere_datastore
+  source                  = "../../../../modules/infra/vmware"
+  ssh_private_key_path    = var.ssh_private_key_path
+  vsphere_datacenter      = var.vsphere_datacenter
+  vsphere_resource_pool   = var.vsphere_resource_pool
+  vsphere_datastore       = var.vsphere_datastore
   vsphere_virtual_machine = var.vsphere_virtual_machine
-  vsphere_network = var.vsphere_network
+  vsphere_network         = var.vsphere_network
 }
 
 module "rke" {
@@ -24,9 +24,9 @@ module "rke" {
 
   rancher_nodes = [for instance_ips in module.upstream-cluster.vsphere_virtual_machine[*][0] :
     {
-      public_ip  = instance_ips
-      roles = ["etcd", "controlplane", "worker"]
-      
+      public_ip = instance_ips
+      roles     = ["etcd", "controlplane", "worker"]
+
     }
   ]
 }
@@ -44,13 +44,13 @@ provider "kubernetes" {
 }
 
 module "rancher_install" {
-  source = "../../../../modules/rancher"
-  depends_on = [module.rke]
-  rancher_hostname = join(".", ["rancher", module.upstream-cluster.rancher_ip, "sslip.io"])
-  rancher_replicas  = 1
+  source                     = "../../../../modules/rancher"
+  depends_on                 = [module.rke]
+  rancher_hostname           = join(".", ["rancher", module.upstream-cluster.rancher_ip, "sslip.io"])
+  rancher_replicas           = 1
   rancher_bootstrap_password = "changeme"
   providers = {
     kubernetes = kubernetes
-    helm = helm
+    helm       = helm
   }
 }
