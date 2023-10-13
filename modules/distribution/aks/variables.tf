1442af1a6a56c6f3ee3a03f4e64d7fe07ddd7f27
Murali Paluru <leodotcloud@gmail.com>
2023-10-13 21:17:02 +0530

    Add AKS support

---
+++ modules/distribution/aks/variables.tf
@@ -0,0 +1,235 @@
+variable "azure_subscription_id" {
+  type        = string
+  description = "Azure subscription id"
+  default     = null
+}
+
+variable "azure_subscription_tenant_id" {
+  type        = string
+  description = "Azure subscription tenant id"
+  default     = null
+}
+
+variable "azure_service_principal_appid" {
+  type        = string
+  description = "Azure service principal app id"
+  default     = null
+}
+
+variable "azure_service_principal_password" {
+  type        = string
+  description = "Azure service principal password"
+  default     = null
+  sensitive   = true
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix added to names of all resources"
+  default     = "rancher-terraform"
+}
+
+variable "azure_region" {
+  type        = string
+  description = "Azure region used for all resources"
+  default     = "westus3"
+  validation {
+    condition = contains([
+      "australiacentral",
+      "australiacentral2",
+      "australiaeast",
+      "australiasoutheast",
+      "brazilsouth",
+      "brazilsoutheast",
+      "canadacentral",
+      "canadaeast",
+      "centralindia",
+      "centralus",
+      "eastasia",
+      "eastus",
+      "eastus2",
+      "francecentral",
+      "francesouth",
+      "germanynorth",
+      "germanywestcentral",
+      "japaneast",
+      "japanwest",
+      "koreacentral",
+      "koreasouth",
+      "northcentralus",
+      "northeurope",
+      "norwayeast",
+      "norwaywest",
+      "polandcentral",
+      "qatarcentral",
+      "southafricanorth",
+      "southafricawest",
+      "southcentralus",
+      "southindia",
+      "southeastasia",
+      "swedencentral",
+      "switzerlandnorth",
+      "switzerlandwest",
+      "uaecentral",
+      "uaenorth",
+      "uksouth",
+      "ukwest",
+      "usgovarizona",
+      "usgovtexas",
+      "usgovvirginia",
+      "westcentralus",
+      "westeurope",
+      "westindia",
+      "westus",
+      "westus2",
+      "westus3",
+    ], var.azure_region)
+    error_message = "invalid region specified"
+  }
+}
+
+variable "node_count" {
+  type        = number
+  description = "Number of nodes to create in the default node pool"
+  default     = 3
+  nullable    = false
+}
+
+variable "vm_size" {
+  type        = string
+  description = "VM Size for the default node pool"
+  default     = "Standard_DS2_v2"
+  nullable    = false
+  validation {
+    condition = contains([
+      "Basic_A4",
+      "Standard_A0",
+      "Standard_A1",
+      "Standard_A1_v2",
+      "Standard_A2",
+      "Standard_A2_v2",
+      "Standard_A3",
+      "Standard_A4",
+      "Standard_A4_v2",
+      "Standard_A5",
+      "Standard_A6",
+      "Standard_A7",
+      "Standard_A8",
+      "Standard_A9",
+      "Standard_A10",
+      "Standard_B1s",
+      "Standard_B1ms",
+      "Standard_B2s",
+      "Standard_B2ms",
+      "Standard_B4s",
+      "Standard_B8s",
+      "Standard_B16s",
+      "Standard_B32s",
+      "Standard_B64s",
+      "Standard_DS1",
+      "Standard_DS1_v2",
+      "Standard_DS2",
+      "Standard_DS2_v2",
+      "Standard_DS3",
+      "Standard_DS3_v2",
+      "Standard_DS4",
+      "Standard_DS4_v2",
+      "Standard_DS5",
+      "Standard_DS5_v2",
+      "Standard_DS11",
+      "Standard_DS11_v2",
+      "Standard_DS12",
+      "Standard_DS12_v2",
+      "Standard_DS13",
+      "Standard_DS13_v2",
+      "Standard_DS14",
+      "Standard_DS14_v2",
+      "Standard_DS15",
+      "Standard_DS15_v2",
+      "Standard_D1",
+      "Standard_D1_v2",
+      "Standard_D2",
+      "Standard_D2_v2",
+      "Standard_D3",
+      "Standard_D3_v2",
+      "Standard_D4",
+      "Standard_D4_v2",
+      "Standard_D5",
+      "Standard_D5_v2",
+      "Standard_D11",
+      "Standard_D11_v2",
+      "Standard_D12",
+      "Standard_D12_v2",
+      "Standard_D13",
+      "Standard_D13_v2",
+      "Standard_D14",
+      "Standard_D14_v2",
+      "Standard_D15",
+      "Standard_D15_v2",
+      "Standard_E1s",
+      "Standard_E1ms",
+      "Standard_E2s",
+      "Standard_E2ms",
+      "Standard_E4s",
+      "Standard_E8s",
+      "Standard_E16s",
+      "Standard_E32s",
+      "Standard_E64s",
+      "Standard_F1s",
+      "Standard_F1ms",
+      "Standard_F2s",
+      "Standard_F2ms",
+      "Standard_F4s",
+      "Standard_F8s",
+      "Standard_F16s",
+      "Standard_F32s",
+      "Standard_F64s",
+      "Standard_G1s",
+      "Standard_G1ms",
+      "Standard_G2s",
+      "Standard_G2ms",
+      "Standard_G4s",
+      "Standard_G8s",
+      "Standard_G16s",
+      "Standard_G32s",
+      "Standard_G64s",
+      "Standard_H1s",
+      "Standard_H1ms",
+      "Standard_H2s",
+      "Standard_H2ms",
+      "Standard_H4s",
+      "Standard_H8s",
+      "Standard_H16s",
+      "Standard_H32s",
+      "Standard_H64s",
+      "Standard_NC6s_v2",
+      "Standard_NC12s_v2",
+      "Standard_NC24s_v2",
+      "Standard_NC24rs_v2",
+      "Standard_ND4s_v2",
+      "Standard_ND6s_v2",
+      "Standard_ND12s_v2",
+      "Standard_NV6s",
+      "Standard_NV12s",
+    ], var.vm_size)
+    error_message = "invalid vm size specified"
+  }
+}
+
+variable "kube_config_path" {
+  description = "The path to write the kubeconfig for the AKS cluster"
+  type        = string
+  default     = null
+}
+
+variable "kube_config_filename" {
+  description = "Filename to write the kue config"
+  type        = string
+  default     = null
+}
+
+variable "default_node_pool_name" {
+  description = "Customize the defaul node pool name"
+  type        = string
+  default     = "defaultnp"
+}
---
+++ recipes/upstream/azure/aks/main.tf
@@ -0,0 +1,67 @@
+module "aks" {
+  source = "../../../../modules/distribution/aks"
+
+  azure_subscription_id            = var.azure_subscription_id
+  azure_subscription_tenant_id     = var.azure_subscription_tenant_id
+  azure_service_principal_appid    = var.azure_service_principal_appid
+  azure_service_principal_password = var.azure_service_principal_password
+
+  prefix               = var.prefix
+  azure_region         = var.azure_region
+  node_count           = var.node_count
+  vm_size              = var.vm_size
+  kube_config_filename = var.kube_config_filename
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = module.aks.kubeconfig_file_location
+  }
+}
+
+resource "helm_release" "ingress-nginx" {
+  depends_on       = [module.aks.dependency]
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  create_namespace = true
+  namespace        = "ingress-nginx"
+  timeout          = 600
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+    value = "/healthz"
+  }
+}
+
+provider "kubernetes" {
+  config_path = module.aks.kubeconfig_file_location
+}
+
+data "kubernetes_service" "ingress-nginx-controller-svc" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = helm_release.ingress-nginx.namespace
+  }
+}
+
+module "rancher_install" {
+  source                     = "../../../../modules/rancher"
+  kubeconfig_file            = module.aks.kubeconfig_file_location
+  rancher_hostname           = join(".", ["rancher", data.kubernetes_service.ingress-nginx-controller-svc.status.0.load_balancer.0.ingress[0].ip, "sslip.io"])
+  rancher_replicas           = min(var.rancher_replicas, var.node_count)
+  rancher_bootstrap_password = var.rancher_bootstrap_password
+  rancher_version            = var.rancher_version
+  wait                       = var.wait
+  rancher_additional_helm_values = [
+    "ingress.ingressClassName: nginx",
+  ]
+}
