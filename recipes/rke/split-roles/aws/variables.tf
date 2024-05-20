102b75ac7deb1688edf73f22f4e94420a7f30cee
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-05-20 07:55:43 +0200

    Organized variable parameters and ensured all variables have type and default values
    - Sorted parameters within each variable block alphabetically.
    - Added missing type and default values to variables where necessary.

--- recipes/rke/split-roles/aws/variables.tf
+++ recipes/rke/split-roles/aws/variables.tf
@@ -1,71 +1,75 @@
 variable "aws_access_key" {
-  type        = string
+  default     = null
   description = "Enter your AWS access key"
+  type        = string
 }
 
 variable "aws_secret_key" {
-  type        = string
+  default     = null
   description = "Enter your AWS secret key"
   sensitive   = true
+  type        = string
 }
 
 variable "aws_region" {
-  type        = string
+  default     = "us-west-2"
   description = "AWS region used for all resources"
+  type        = string
 }
 
 variable "vpc_zone" {
-  type        = string
-  description = "VPC zone"
   default     = null
+  description = "VPC zone"
+  type        = string
 }
 
 variable "subnet_id" {
-  type        = string
-  description = "VPC Subnet ID to create the instance(s) in"
   default     = null
+  description = "VPC Subnet ID to create the instance(s) in"
+  type        = string
 }
 
 variable "vpc_id" {
-  type        = string
-  description = "VPC ID to create the instance(s) in"
   default     = null
+  description = "VPC ID to create the instance(s) in"
+  type        = string
 }
 
 variable "create_security_group" {
-  type        = bool
-  description = "Should create the security group associated with the instance(s)"
   default     = true
+  description = "Should create the security group associated with the instance(s)"
+  type        = bool
 }
 
 # TODO: Add a check based on above value
 variable "instance_security_group" {
-  type        = string
-  description = "Provide a pre-existing security group ID"
   default     = null
+  description = "Provide a pre-existing security group ID"
+  type        = string
 }
 
 variable "instance_security_group_name" {
-  type        = string
-  description = "Provide a pre-existing security group name"
   default     = null
+  description = "Provide a pre-existing security group name"
+  type        = string
 }
 
 variable "prefix" {
-  type        = string
+  default     = null
   description = "Prefix added to names of all resources"
+  type        = string
 }
 
 variable "master_nodes_count" {
-  type        = number
-  description = "Number of master nodes to create"
   default     = 1
+  description = "Number of master nodes to create"
+  type        = number
 }
 
 variable "worker_nodes_count" {
-  type        = number
-  description = "Number of worker nodes to create"
   default     = 1
+  description = "Number of worker nodes to create"
+  type        = number
 }
 
 variable "kube_config_path" {
@@ -87,113 +91,116 @@ variable "kubernetes_version" {
 }
 
 variable "install_docker" {
-  type        = bool
-  description = "Should install docker while creating the instance"
   default     = true
+  description = "Should install docker while creating the instance"
+  type        = bool
 }
 
 variable "docker_version" {
-  type        = string
-  description = "Docker version to install on nodes"
   default     = "23.0.6"
+  description = "Docker version to install on nodes"
+  type        = string
 }
 
 variable "create_ssh_key_pair" {
-  type        = bool
-  description = "Specify if a new SSH key pair needs to be created for the instances"
   default     = false
+  description = "Specify if a new SSH key pair needs to be created for the instances"
+  type        = bool
 }
 
 variable "ssh_key_pair_name" {
-  type        = string
-  description = "Specify the SSH key name to use (that's already present in AWS)"
   default     = null
+  description = "Specify the SSH key name to use (that's already present in AWS)"
+  type        = string
 }
 
 variable "ssh_key_pair_path" {
-  type        = string
-  description = "Path to the SSH private key used as the key pair (that's already present in AWS)"
   default     = null
+  description = "Path to the SSH private key used as the key pair (that's already present in AWS)"
+  type        = string
 }
 
 variable "ssh_key" {
-  type        = string
-  description = "Contents of the private key to connect to the instances."
   default     = null
+  description = "Contents of the private key to connect to the instances."
   sensitive   = true
+  type        = string
 }
 
 variable "bastion_host" {
+  default     = null
+  description = "Bastion host configuration to access the RKE nodes"
+
   type = object({
     address      = string
     user         = string
     ssh_key_path = string
     ssh_key      = string
   })
-  default     = null
-  description = "Bastion host configuration to access the RKE nodes"
 }
 
 variable "ssh_username" {
-  type        = string
-  description = "Username used for SSH with sudo access"
   default     = "ubuntu"
+  description = "Username used for SSH with sudo access"
+  type        = string
 }
 
 variable "master_nodes_instance_type" {
-  type        = string
-  description = "Instance type used for all master nodes"
   default     = "t3.medium"
+  description = "Instance type used for all master nodes"
+  type        = string
 }
 
 variable "master_nodes_instance_disk_size" {
-  type        = string
-  description = "Disk size used for all master nodes (in GB)"
   default     = "80"
+  description = "Disk size used for all master nodes (in GB)"
+  type        = string
 }
 
 variable "worker_nodes_instance_type" {
-  type        = string
-  description = "Instance type used for all worker nodes"
   default     = "t3.large"
+  description = "Instance type used for all worker nodes"
+  type        = string
 }
 
 variable "worker_nodes_instance_disk_size" {
-  type        = string
-  description = "Disk size used for all worker nodes (in GB)"
   default     = "80"
+  description = "Disk size used for all worker nodes (in GB)"
+  type        = string
 }
 
 variable "dependency" {
-  description = "An optional variable to add a dependency from another resource (not used)"
   default     = null
+  description = "An optional variable to add a dependency from another resource (not used)"
+  type        = bool
 }
 
 variable "master_nodes_iam_instance_profile" {
-  description = "Specify IAM instance profile to attach to master nodes"
   default     = null
+  description = "Specify IAM instance profile to attach to master nodes"
   type        = string
 }
 
 variable "worker_nodes_iam_instance_profile" {
-  description = "Specify IAM instance profile to attach to worker nodes"
   default     = null
+  description = "Specify IAM instance profile to attach to worker nodes"
   type        = string
 }
 
 variable "tags" {
+  default     = {}
   description = "User-provided tags for the resources"
   type        = map(string)
-  default     = {}
 }
 
 variable "cloud_provider" {
+  default     = null
   description = "Specify the cloud provider name"
   type        = string
-  default     = null
 }
 
 variable "create_kubeconfig_file" {
-  description = "Boolean flag to generate a kubeconfig file (mostly used for dev only)"
   default     = true
+  description = "Boolean flag to generate a kubeconfig file (mostly used for dev only)"
+  type        = bool
 }
