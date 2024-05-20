08d63b38796be462fb4101736194f6c03fe75349
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-05-20 13:04:26 +0200

    Improve multiple variable definitions, order parameters inside definitions

--- recipes/upstream/vmware/rke/variables.tf
+++ recipes/upstream/vmware/rke/variables.tf
@@ -1,43 +1,73 @@
-variable "vsphere_server" {
-  description = "Add the vSphere hostname."
-}
-
-variable "vsphere_server_allow_unverified_ssl" {
-  description = "Allow use of unverified SSL certificates (Ex: Self signed)"
-}
-variable "vsphere_user" {
-  description = "Add your vSphere username."
-}
-
-variable "vsphere_password" {
-  description = "Add your vSphere password for the above mentioned username."
-}
-
 variable "instance_count" {
-  type        = number
-  description = "Number of instances to create"
   default     = 3
+  description = "Number of instances to create"
+  type        = number
 }
 
 variable "ssh_private_key_path" {
+  default     = null
   description = "Add your SSH private key path here."
+  type        = string
+}
+
+variable "ssh_username" {
+  default     = null
+  description = "Username used for SSH with sudo access"
+  type        = string
 }
 
 variable "vsphere_datacenter" {
+  default     = null
   description = "vSphere Datacenter details."
+  type        = string
 }
 
 variable "vsphere_datastore" {
+  default     = null
   description = "Datastore used for storing VM data."
-}
-
-variable "vsphere_resource_pool" {
-  description = "Available resourcepool on the host."
-}
-
-variable "vsphere_virtual_machine" {
-  description = "Virtual Machine template name"
+  type        = string
 }
 
 variable "vsphere_network" {
+  default     = null
+  description = "Network to which the VM is connected."
+  type        = string
 }
+
+variable "vsphere_password" {
+  default     = null
+  description = "Add your vSphere password for the above-mentioned username."
+  type        = string
+  sensitive   = true
+}
+
+variable "vsphere_resource_pool" {
+  default     = null
+  description = "Available resource pool on the host."
+  type        = string
+}
+
+variable "vsphere_server" {
+  default     = null
+  description = "Add the vSphere hostname."
+  type        = string
+}
+
+variable "vsphere_server_allow_unverified_ssl" {
+  default     = null
+  description = "Allow use of unverified SSL certificates (Ex: Self signed)"
+  type        = bool
+}
+
+variable "vsphere_user" {
+  default     = null
+  description = "Add your vSphere username."
+  type        = string
+}
+
+variable "vsphere_virtual_machine" {
+  default     = null
+  description = "Virtual Machine template name"
+  type        = string
+}
+
