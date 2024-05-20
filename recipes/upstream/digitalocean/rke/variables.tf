2109bc99254ebd8e992d1d4a3a482316b02823eb
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2024-05-20 11:21:24 +0200

    Organized variable parameters and ensured all variables have type and default values
    - Sorted parameters within each variable block alphabetically.
    - Added missing type and default values to variables where necessary.

--- recipes/upstream/digitalocean/rke/variables.tf
+++ recipes/upstream/digitalocean/rke/variables.tf
@@ -1,71 +1,73 @@
 variable "do_token" {
-  type        = string
-  description = "DigitalOcean Authentication Token"
   default     = null
+  description = "DigitalOcean Authentication Token"
   sensitive   = true
+  type        = string
 }
 
 variable "droplet_count" {
-  type        = number
-  description = "Number of droplets to create"
   default     = 3
+  description = "Number of droplets to create"
   nullable    = false
+  type        = number
 }
 
 variable "droplet_size" {
-  type        = string
-  description = "Size used for all droplets"
   default     = "s-2vcpu-4gb"
+  description = "Size used for all droplets"
   nullable    = false
+  type        = string
 
 }
 
 variable "prefix" {
-  type        = string
-  description = "Prefix added to names of all resources"
   default     = "rancher-terraform"
+  description = "Prefix added to names of all resources"
+  type        = string
 }
 
 variable "tag_begin" {
-  type        = number
-  description = "Tag from this number when module is called more than once"
   default     = 1
+  description = "Tag from this number when module is called more than once"
+  type        = number
 }
 
 variable "user_tag" {
-  type        = string
+  default     = 1
   description = "FirstInitialLastname of user"
   nullable    = false
+  type        = string
 }
 
 variable "create_ssh_key_pair" {
-  type        = bool
-  description = "Specify if a new SSH key pair needs to be created for the instances"
   default     = false
+  description = "Specify if a new SSH key pair needs to be created for the instances"
   nullable    = false
+  type        = bool
 }
 
 variable "ssh_key_pair_name" {
-  type        = string
-  description = "Specify the SSH key name to use (that's already present in DigitalOcean)"
   default     = null
+  description = "Specify the SSH key name to use (that's already present in DigitalOcean)"
+  type        = string
 }
 
 variable "ssh_key_pair_path" {
-  type        = string
+  default     = null
   description = "Path to the SSH private key used as the key pair (that's already present in DigitalOcean)"
-  default     = null
-}
-variable "ssh_private_key_path" {
   type        = string
-  description = "Path to write the generated SSH private key"
+}
+
+variable "ssh_private_key_path" {
   default     = null
+  description = "Path to write the generated SSH private key"
+  type        = string
 }
 
 variable "region" {
-  type        = string
-  description = "Region that droplets will be deployed to"
   default     = "sfo3"
+  description = "Region that droplets will be deployed to"
+  type        = string
 
   validation {
     condition = contains([
@@ -86,43 +88,44 @@ variable "region" {
 }
 
 variable "ssh_username" {
-  description = "The user account used to connect to droplets via ssh"
-  type        = string
   default     = "root"
+  description = "The user account used to connect to droplets via ssh"
   nullable    = false
+  type        = string
 }
 
 variable "kube_config_path" {
+  default     = null
   description = "The path to write the kubeconfig for the RKE cluster"
   type        = string
-  default     = null
 }
 
 variable "kube_config_filename" {
+  default     = null
   description = "Filename to write the kube config"
   type        = string
-  default     = null
 }
 
 variable "kubernetes_version" {
-  type        = string
-  description = "Kubernetes version to use for the RKE cluster"
   default     = null
+  description = "Kubernetes version to use for the RKE cluster"
+  type        = string
 }
 
 variable "rancher_password" {
-  description = "Password to use for bootstrapping Rancher (min 12 characters)"
   default     = "initial-admin-password"
+  description = "Password to use for bootstrapping Rancher (min 12 characters)"
   type        = string
 }
 
 variable "rancher_version" {
-  description = "Rancher version to install"
   default     = null
+  description = "Rancher version to install"
   type        = string
 }
 
 variable "wait" {
   description = "An optional wait before installing the Rancher helm chart"
   default     = "20s"
+  type        = string
 }
