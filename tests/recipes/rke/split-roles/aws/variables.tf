a2e05767c796da95811adaec85d32bc0cd69518e
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-07-02 12:24:11 +0200

    Reviewed tests for AWS EC2 x RKE (split-roles)

--- tests/recipes/rke/split-roles/aws/variables.tf
+++ tests/recipes/rke/split-roles/aws/variables.tf
@@ -1,11 +1,19 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS access key used to create infrastructure"
-  default     = null
+variable "prefix" {
+  default = "ec2-test"
 }
 
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS secret key used to create AWS infrastructure"
-  default     = null
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "server_nodes_count" {
+  default = 3
+}
+
+variable "worker_nodes_count" {
+  default = 3
+}
+
+variable "ssh_username" {
+  default = "ubuntu"
 }
