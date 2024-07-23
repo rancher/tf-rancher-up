db53e59d4b5cb89f5288894bb37eab23acf691a1
Murali Paluru <leodotcloud@gmail.com>
2024-07-23 16:19:33 +0530

    add standalone rke recipe

---
+++ recipes/standalone/aws/rke/variables.tf
@@ -0,0 +1,160 @@
+variable "dependency" {
+  description = "An optional variable to add a dependency from another resource (not used)"
+  default     = null
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key used to create infrastructure"
+  default     = null
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key used to create AWS infrastructure"
+  default     = null
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region used for all resources"
+  default     = "us-east-1"
+
+  validation {
+    condition = contains([
+      "us-east-2",
+      "us-east-1",
+      "us-west-1",
+      "us-west-2",
+      "af-south-1",
+      "ap-east-1",
+      "ap-south-2",
+      "ap-southeast-3",
+      "ap-southeast-4",
+      "ap-south-1",
+      "ap-northeast-3",
+      "ap-northeast-2",
+      "ap-southeast-1",
+      "ap-southeast-2",
+      "ap-northeast-1",
+      "ca-central-1",
+      "ca-west-1",
+      "eu-central-1",
+      "eu-west-1",
+      "eu-west-2",
+      "eu-south-1",
+      "eu-west-3",
+      "eu-south-2",
+      "eu-north-1",
+      "eu-central-2",
+      "il-central-1",
+      "me-south-1",
+      "me-central-1",
+      "sa-east-1",
+    ], var.aws_region)
+    error_message = "Invalid Region specified!"
+  }
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix added to names of all resources"
+  default     = null
+}
+
+variable "instance_count" {
+  type        = number
+  description = "Number of EC2 instances to create"
+  default     = null
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type used for all EC2 instances"
+  default     = null
+}
+
+variable "instance_disk_size" {
+  type        = string
+  description = "Specify root disk size (GB)"
+  default     = null
+}
+
+variable "kube_config_path" {
+  description = "The path to write the kubeconfig for the RKE cluster"
+  type        = string
+  default     = null
+}
+
+variable "kube_config_filename" {
+  description = "Filename to write the kube config"
+  type        = string
+  default     = null
+}
+
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version to use for the RKE cluster"
+  default     = null
+}
+
+variable "install_docker" {
+  type        = bool
+  description = "Should install docker while creating the instance"
+  default     = true
+}
+
+variable "docker_version" {
+  type        = string
+  description = "Docker version to install on nodes"
+  default     = "20.10"
+}
+
+variable "create_ssh_key_pair" {
+  type        = bool
+  description = "Specify if a new SSH key pair needs to be created for the instances"
+  default     = null
+}
+
+variable "ssh_key_pair_name" {
+  type        = string
+  description = "Specify the SSH key name to use (that's already present in AWS)"
+  default     = null
+}
+
+variable "ssh_key_pair_path" {
+  type        = string
+  description = "Path to the SSH private key used as the key pair (that's already present in AWS)"
+  default     = null
+}
+
+variable "ssh_username" {
+  type        = string
+  description = "Username used for SSH with sudo access"
+  default     = "ubuntu"
+}
+
+variable "spot_instances" {
+  type        = bool
+  description = "Use spot instances"
+  default     = null
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "VPC Subnet ID to create the instance(s) in"
+  default     = null
+}
+
+variable "create_security_group" {
+  type        = bool
+  description = "Should create the security group associated with the instance(s)"
+  default     = null
+}
+
+# TODO: Add a check based on above value
+variable "instance_security_group" {
+  type        = string
+  description = "Provide a pre-existing security group ID"
+  default     = null
+}
