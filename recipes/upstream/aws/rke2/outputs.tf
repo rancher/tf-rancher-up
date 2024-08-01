91eb1c8ddc7f3d8a03e3c6a4502e56f10d05a0ef
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-08-01 09:44:33 +0200

    Review of AWS RKE2 recipe

--- recipes/upstream/aws/rke2/outputs.tf
+++ recipes/upstream/aws/rke2/outputs.tf
@@ -1,25 +1,19 @@
-output "instances_public_ip" {
-  value = concat([module.rke2_first_server.instances_public_ip], [module.rke2_additional_servers.instances_public_ip])
-}
+# Uncomment for debugging purposes
+#output "rke2_first_server_config_file" {
+#  value = nonsensitive(module.rke2_first.rke2_user_data)
+#}
 
-output "instances_private_ip" {
-  value = concat([module.rke2_first_server.instances_private_ip], [module.rke2_additional_servers.instances_private_ip])
-}
-
-output "rancher_hostname" {
-  value = local.rancher_hostname
-}
+# Uncomment for debugging purposes
+#output "rke2_additional_servers_config_file" {
+#  value = nonsensitive(module.rke2_additional.rke2_user_data)
+#}
 
 output "rancher_url" {
-  value = "https://${local.rancher_hostname}"
+  description = "Rancher URL"
+  value       = "https://${module.rancher_install.rancher_hostname}"
 }
 
 output "rancher_password" {
-  value = var.rancher_bootstrap_password
-}
-
-output "rancher_admin_token" {
-  description = "Rancher API token for the admin user"
-  value       = module.rancher_install.rancher_admin_token
-  sensitive   = true
+  description = "Rancher Initial Custom Password"
+  value       = var.rancher_password
 }
