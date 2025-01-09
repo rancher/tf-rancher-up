568bc104f0bb2a83e4ec71cc873509402974f1df
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.local>
2025-01-09 11:13:01 +0100

    Added logic to handle finding a new image; VM update blocked in this case

--- recipes/upstream/google-cloud/k3s/outputs.tf
+++ recipes/upstream/google-cloud/k3s/outputs.tf
@@ -1,9 +1,9 @@
 output "instances_private_ip" {
-  value = concat([module.k3s_first_server.instances_private_ip], [module.k3s_additional_servers.instances_private_ip])
+  value = concat([module.k3s_first_server.instances_private_ip], [module.k3s_additional_servers.instances_private_ip], [module.k3s_additional_workers.instances_private_ip])
 }
 
 output "instances_public_ip" {
-  value = concat([module.k3s_first_server.instances_public_ip], [module.k3s_additional_servers.instances_public_ip])
+  value = concat([module.k3s_first_server.instances_public_ip], [module.k3s_additional_servers.instances_public_ip], [module.k3s_additional_workers.instances_public_ip])
 }
 
 output "instances_ssh_username" {
