33dc3e9f45b3172672ab8f56f5078374e323126a
dkeightley <derek@rancher.com>
2025-02-05 14:18:18 +1300

    Update rke recipe

--- modules/distribution/rke/versions.tf
+++ modules/distribution/rke/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     rke = {
       source  = "rancher/rke"
-      version = ">= 1.4.3"
+      version = ">= 1.7.0"
     }
     local = {
       source  = "hashicorp/local"
