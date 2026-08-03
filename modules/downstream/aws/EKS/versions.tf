11f91d8cf649659b26a161f459de2d3c9e12cb12
dkeightley <derek@rancher.com>
2026-08-03 12:38:41 +1200

    Add fallback EKS version from datasource

--- modules/downstream/aws/EKS/versions.tf
+++ modules/downstream/aws/EKS/versions.tf
@@ -4,5 +4,13 @@ terraform {
       source  = "rancher/rancher2"
       version = ">= 8.0.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
   }
 }
--- recipes/downstream/aws/EKS/versions.tf
+++ recipes/downstream/aws/EKS/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "rancher/rancher2"
       version = ">= 8.0.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
   }
 }
